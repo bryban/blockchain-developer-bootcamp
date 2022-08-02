41597443720d090216b12b563b93b36084232a99
Bryan banuelos <bryanbanuelos@Bryans-Air.attlocal.net>
2022-08-02 11:26:43 -0500

    3. TestingContracts

---
+++ test/Token.js
@@ -0,0 +1,43 @@
+const { expect } = require ('chai')
+const { ethers } = require ('hardhat')
+
+const tokens = (n)=> {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', ()=> {
+	let token
+
+	beforeEach( async() => {
+		const Token = await ethers.getContractFactory('Token')
+		token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+	})
+
+	describe('Deployment', () =>{
+
+		const name = 'Dapp University'
+		const symbol = 'DAPP'
+		const decimals = '18'
+		const totalSupply = tokens ('1000000')
+
+		it('Has a correct name', async ()=> {
+			expect(await token.name() ).to.equal(name)
+		})
+
+		it('Has a correct symbol', async ()=> {
+			expect(await token.symbol() ).to.equal(symbol)
+		})
+
+		it('Has correct amount of decimals', async ()=> {
+			expect(await token.decimals() ).to.equal(decimals)
+		})
+
+		it('Has correct total supply', async ()=> {
+			expect(await token.totalSupply() ).to.equal(totalSupply)
+		})
+
+		})
+
+	
+
+} )
