61fd2cae528b3b2398637a37829bceb323922d8f
Bryan banuelos <bryanbanuelos@Bryans-Air.attlocal.net>
2022-08-29 18:17:21 -0500

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -8,6 +10,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
+    localhost:{},
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   },
 };
